1fa6c29b6838079f73ae58ab8a307d4a1a943298
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-05-22 20:05:03 +0300

    added hamburger menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import "./Header.css";
 import logo from "../images/main-logo.jpg";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className="header">
       <div className="logo-title">
         <img src={logo} alt="Greekend Transfers Logo" className="logo" />
-        <div className="title-text">
+        <div>
           <h1 className="site-title">Greekend Transfers</h1>
           <p className="established">est. 2021</p>
         </div>
       </div>
-      <nav className="nav-menu">
-        <a href="#info">Info</a>
-        <a href="#fleet">Fleet</a>
-        <a href="#contact">Contact</a>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+      </div>
+
+      <nav className={`nav-menu ${isMenuOpen ? "open" : ""}`}>
+        <a href="#info" onClick={() => setIsMenuOpen(false)}>Info</a>
+        <a href="#fleet" onClick={() => setIsMenuOpen(false)}>Fleet</a>
+        <a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a>
       </nav>
     </header>
   );
