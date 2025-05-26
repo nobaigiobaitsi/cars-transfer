bc1ce00cc6095c15586603772ef10bc314465edd
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-05-26 11:19:43 +0300

    fixed typos

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
           We’re here to help! Reach out anytime, and we’ll get back to you as soon as possible.
         </p>
         <ul className="contact-info">
-          <li><strong>Phone:</strong> 6972500117, 6940514882</li>
+          <li><strong>Phones:</strong> 6972500117, 6940514882</li>
           <li><strong>Email:</strong> greatkapiris@gmail.com</li>
         </ul>
       </div>
