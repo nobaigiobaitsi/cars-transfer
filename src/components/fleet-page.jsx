e8640590d2ac7ed3fb6720bbdbb9fb3d6cc213d6
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-05-28 21:35:26 +0300

    Started working on the fleet page

--- src/components/fleet-page.jsx
+++ src/components/fleet-page.jsx
@@ -1,1 +1,30 @@
 import "./fleet-page.css";
+
+
+const Fleet = () => {
+    return (
+        <>
+        <div className="fleet-page">
+            <h1>Our Fleet</h1>
+            <p>Here is the selection of vehicles we offer</p>
+            <div className="fleet-grid">
+                <div className="fleet-item">
+                <img src="/images/car1.jpg" alt="Car 1" />
+                <h2>Car Model 1</h2>
+                <p>Description of Car Model 1.</p>
+            </div>
+            <div className="fleet-item">
+                <img src="/images/car2.jpg" alt="Car 2" />
+                <h2>Car Model 2</h2>
+                <p>Description of Car Model 2.</p>
+            </div>
+            <div className="fleet-item">
+                <img src="/images/car3.jpg" alt="Car 3" />
+                <h2>Car Model 3</h2>
+                <p>Description of Car Model 3.</p>
+            </div>
+        </div>
+        </div>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/header.jsx'
-import HeroSection from './components/hero-section.jsx'
-import Info from './components/info.jsx'
-import Fleet from './components/fleet.jsx'
-import Contact from './components/contact.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <HeroSection />
-    {/* <hr className="section-divider" /> */}
-    <Info />
-    {/* <hr className="section-divider" /> */}
-    <Fleet />
-    {/* <hr className="section-divider" /> */}
-    <Contact />
   </StrictMode>
 )
