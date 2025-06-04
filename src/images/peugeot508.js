8140e44b30ba2d535c680b968d1a203ea4675439
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-04 19:18:15 +0300

    Debugged the images carousel.

--- src/images/peugeot508.js
+++ src/images/peugeot508.js
@@ -2,4 +2,6 @@ import image1 from './peugeot2nd.png';
 import image2 from './peugeot3rd.png';
 import image3 from './peugeot4th.png';
 
-export default [image1, image2, image3];
+const peugeot508 = [image1, image2, image3];
+
+export default peugeot508;
