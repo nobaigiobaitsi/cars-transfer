8c1c37af07109fccd7c1b565981be4e23de63ca8
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-06 12:09:31 +0300

    Configured the routes to connect many pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/header.jsx'
-import HeroSection from './components/hero-section.jsx'
-import Info from './components/info.jsx'
-import Fleet from './components/fleet.jsx'
-import Contact from './components/contact.jsx'
+import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <HeroSection />
-    {/* <hr className="section-divider" /> */}
-    <Info />
-    {/* <hr className="section-divider" /> */}
-    <Fleet />
-    {/* <hr className="section-divider" /> */}
-    <Contact />
+    <App />
   </StrictMode>
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Header from '../components/header.jsx'
+import HeroSection from '../components/hero-section.jsx'
+import Info from '../components/info.jsx'
+import Fleet from '../components/fleet.jsx'
+import Contact from '../components/contact.jsx'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Info />
+      <Fleet />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
