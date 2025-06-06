ca9c013fa64b070a0f0e3d89f4a8f8215fe5ef63
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-06 12:18:36 +0300

    Modified the Fleet page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Fleet from "./pages/Fleet";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/fleet">Fleet</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/fleet" element={<Fleet />} />
       </Routes>
     </Router>
   );
