b6e07a6024f851c030d81c46bbcbd768da1811f3
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-10 11:51:40 +0300

    Feat: Added a component to auto-scroll to the top everytime a page changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import Fleet from "./pages/Fleet";
 import Layout from './components/Layout';
 import ContactPage from "./pages/ContactPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/fleet" element={<Layout><Fleet /></Layout>} />
