c6822f6280c02fffa1294d2fbb18717b6321d388
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-13 11:57:32 +0300

    Modified contact section with accurate phone numbers

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
           We’re here to help! Reach out anytime, and we’ll get back to you as soon as possible.
         </p>
         <ul className="contact-info">
-          <li><strong>Phones:</strong> 6972500117, 6940514882</li>
+          <li><strong>Phones:</strong> +30 6972500117, +30 6940514882</li>
           <li><strong>Email:</strong> greatkapiris@gmail.com</li>
           <li>
             <strong>Instagram:</strong>
