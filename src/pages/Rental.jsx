b467e3788e13e092b1e0d0717a30cd4a9c890763
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-18 12:30:37 +0300

    Added correct info and altered some styling in rental page

--- src/pages/Rental.jsx
+++ src/pages/Rental.jsx
@@ -5,27 +5,26 @@ import logo1 from '../images/Motorhome-van.png'
 const Rental = () => {
   return (
     <div className="rental-page">
-      <div className="rental-overlay">
-        <h1 className="rental-title">Motorhome Rental in Greece</h1>
-
-        <p className="rental-subtitle">
-          Discover Greece in total freedom with our fully-equipped motorhome. Perfect for groups, couples, families, or solo travelers.
-        </p>
-        <p className="rental-subtitle">Talk to us and we can help you organise the perfect trip anywhere on Greece. We will give you suggestions and ideas that will assist you in discovering the charms of our beautiful country.</p>
-
+      <div className="rental-top">
+          <div className="rental-overlay">
+            <h1 className="rental-title">Motorhome Rental in Greece</h1>
+            <p className="rental-subtitle">
+              Discover Greece in total freedom with our fully-equipped motorhome. Perfect for groups, couples, families, or solo travelers.
+            </p>
+            <p className="rental-subtitle">Talk to us and we can help you organise the perfect trip anywhere on Greece. We will give you suggestions and ideas that will assist you in discovering the charms of our beautiful country.</p>
+          </div>
+        </div>
         <div className="rental-item">
           <img src={logo1} alt="Motorhome Campervan" className="rental-image" />
           <div className="rental-item-overlay">
             <h2 className="rental-item-title">Your Home on the Road</h2>
-            <p className="rental-item-description">
-              Enjoy comfort and flexibility with a spacious motorhome that includes sleeping space for 5, kitchen, bathroom, and more.
-            </p>
+            <p className="rental-item-description">Based on the Renault Master chassis -known for its reliability and comfortable driving experience- this makes a popular choice for recreational vehicle enthusiasts. The defining feature is the double bed situated above the driver's cab, maximizing space utilization. With that in mind, 6 people can comfortably sleep in. The living space is designed for comfort and relaxation featuring a large table, seating, and ample storage. A kitchen with multiple hob burners, a sink, a refrigerator/freezer, and sufficient counter space for meal preparation. A bathroom with a shower, a toilet, and a sink offer convenience and comfort. Plenty of storage space is available throughout the motorhome, including overhead lockers, drawers, and wardrobe space. To sum up, you will not be uncomfortable since it is spacious and the ride is great thanks to the sophisticated design.</p>
           </div>
         </div>
 
-        <h1 className="rental-subtitle" id='rental-sub-header'><strong>What's Included</strong></h1>
+        <h1 className="rental-subtitle" id='rental-sub-header'><strong>In a few words</strong></h1>
         <ul className="rental-features">
-          <li>Sleeps up to 4 people</li>
+          <li>Sleeps up to 6 people</li>
           <li>Fully-equipped kitchenette</li>
           <li>Bathroom with shower</li>
           <li>Air conditioning & heating</li>
@@ -37,7 +36,6 @@ const Rental = () => {
           </p>
         </div>
       </div>
-    </div>
   );
 };
 
